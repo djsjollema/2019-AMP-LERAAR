066172a448f07cfab1edfa6135006d0ad0f2befe
Jelle Sjollema <=>
2019-10-08 20:56:52 +0200

    rope02

--- html/Rope/script.js
+++ html/Rope/script.js
@@ -10,15 +10,27 @@ canvas.height = height;
 //let stick = new Stick();
 //stick.draw(context)
 
-let A = new dPoint(new Vector2d(100,100),new Vector2d(5,7),new Vector2d(0,1),10,"yellow","A");
+let points = [];
+
+for(let i=0; i<20; i++){
+  let A = new dPoint(new Vector2d(getRND(width),getRND(height)),new Vector2d(getRND(10),0),new Vector2d(0,1),10,"black","" + i);
+  points.push(A)
+};
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
-  A.update()
-  A.draw(context)
+  for(let i=0; i<points.length;i++){
+    points[i].update();
+    points[i].draw(context)
+  }
+
   //stick.update();
   //stick.draw(context);
 }
 
 animate();
+
+function getRND(max){
+  return Math.floor(Math.random()*max)
+}
