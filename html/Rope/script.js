dbe4c9f0bda4c8782f802d937f7e9b8b46583164
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-10-09 13:35:42 +0200

    transform

--- html/Rope/script.js
+++ html/Rope/script.js
@@ -7,18 +7,37 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-//let stick = new Stick();
-//stick.draw(context)
+let stick = new Stick();
+stick.draw(context)
 
-let A = new dPoint(new Vector2d(100,100),new Vector2d(5,7),new Vector2d(0,1),10,"yellow","A");
+// let dPoints = []
+//
+// for(let i=0; i<100; i++){
+//   let A = new dPoint(
+//     new Vector2d( getRND(width), getRND(height)),
+//     new Vector2d(5,7),
+//     new Vector2d(0,1),
+//     10,
+//     "blue",
+//     "" + i);
+//     dPoints.push(A);
+// }
+
+
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
-  A.update()
-  A.draw(context)
-  //stick.update();
-  //stick.draw(context);
+  // for(let i = 0; i< dPoints.length; i++){
+  //   dPoints[i].update()
+  //   dPoints[i].draw(context)
+  // }
+  stick.update();
+  stick.draw(context);
 }
 
 animate();
+
+function getRND(max){
+  return Math.floor(Math.random()*max);
+}
