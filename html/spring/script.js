f8046e1c2106754f6cce4df395c5f8e78d783244
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-10-15 15:39:02 +0200

    spring_01

--- html/spring/script.js
+++ html/spring/script.js
@@ -7,17 +7,17 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(100,100),5,"yellow","A",true)
-let B = new Point(new Vector2d(150,120),5,"red","B",true)
+let A = new Point(new Vector2d(100,100),20,"yellow","A",true)
+let B = new Point(new Vector2d(650,120),20,"red","B",true)
 
 let spring = new Spring(A,B);
 
 function animate() {
   context.clearRect(0,0,width,height);
   requestAnimationFrame(animate);
-  spring.startVector.draw(context,spring.beginPoint.position.dx,spring.beginPoint.position.dy,1)
   spring.draw(context);
   spring.update();
+  console.log(spring.length)
 }
 
 animate();
