c53b86a2498d0623d7ba47073c6060d17747295c
djsjollema <j.sjollema@ma-web.nl>
2019-11-11 14:19:34 +0100

    spring2 --final

--- html/spring2/script.js
+++ html/spring2/script.js
@@ -7,8 +7,8 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(500,100),25,"red","A",true);
-let B = new Point(new Vector2d(650,150),25,"blue","B",true);
+let A = new Point(new Vector2d(100,100),25,"red","A",true);
+let B = new Point(new Vector2d(250,150),25,"blue","B",true);
 
 let spring = new Spring(A,B,0.9,0.8,new Vector2d(0,1));
 
