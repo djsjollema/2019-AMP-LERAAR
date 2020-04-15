5096b5df6bd7c3c90b85bd8a983d0db43f155536
Jelle Sjollema <=>
2020-04-15 12:21:31 +0200

    23 --finalLesson

--- html/23idealGas/script.js
+++ html/23idealGas/script.js
@@ -19,8 +19,38 @@ function animate(){
     molecules.map((otherMolecule)=>{
       if(molecule.index != otherMolecule.index){
         let distance = new Vector2d(1,1);
-        distance.differenceVector(otherMolecule.pos,molecule.pos);
-        distance.draw(context,molecule.pos,1,"rgba(255,255,255,0.1)")
+        molecule.nextPos.sumVector(molecule.pos,molecule.vel)
+        distance.differenceVector(otherMolecule.pos,molecule.nextPos);
+
+        if(distance.magnitude < molecule.radius + otherMolecule.radius){
+          molecule.rad.dx = distance.dx;
+          molecule.rad.dy = distance.dy;
+          otherMolecule.rad.dx = distance.dx;
+          otherMolecule.rad.dy = distance.dy;
+
+          molecule.rad.magnitude = 1;
+          otherMolecule.rad.magnitude = 1;
+
+          molecule.tan.dx = -molecule.rad.dy;
+          molecule.tan.dy = molecule.rad.dx;
+
+          otherMolecule.tan.dx = -otherMolecule.rad.dy;
+          otherMolecule.tan.dy = otherMolecule.rad.dx;
+
+          let temp = new Vector2d(1,1);
+          temp.dx = molecule.rad.dx;
+          temp.dy = molecule.rad.dy;
+
+          molecule.rad.dx = otherMolecule.rad.dx;
+          molecule.rad.dy = otherMolecule.rad.dx;
+
+          otherMolecule.rad.dx = temp.dx;
+          otherMolecule.rad.dy = temp.dy;
+
+          molecule.vel.sumVector(molecule.rad,molecule.tan);
+          otherMolecule.vel.sumVector(otherMolecule.rad,otherMolecule.tan);
+
+        }
       }
 
     });
@@ -31,10 +61,10 @@ animate();
 
 function fillMoleculeArray(){
   let array = [];
-  let numberOfMolecules = 10;
+  let numberOfMolecules = 100;
   let collumnWidth = 100;
   let rowHeight = 100;
-  let moleculesOnARow = 5;
+  let moleculesOnARow = 15;
 
   for(let i=0; i<numberOfMolecules; i++){
     let x = collumnWidth/2 + i%moleculesOnARow * collumnWidth;
@@ -42,6 +72,8 @@ function fillMoleculeArray(){
     let molecule = new dPoint(new Vector2d(x,y),new Vector2d(getRandomNumber(-2,2),getRandomNumber(-2,2)),new Vector2d(0,0),20,"yellow","" + i);
     molecule.nextPos = new Vector2d(1,1);
     molecule.index = i;
+    molecule.rad = new Vector2d(1,1);
+    molecule.tan = new Vector2d(1,1);
     array.push(molecule);
   }
 
