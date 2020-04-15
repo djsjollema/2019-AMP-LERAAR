e3048487f7af819282c03fb926f9b76ae32d2be5
Jelle Sjollema <=>
2020-04-15 11:58:45 +0200

    23 --start

--- html/23idealGas/script.js
+++ html/23idealGas/script.js
@@ -7,80 +7,48 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let atoms = createAtoms();
+let molecules = fillMoleculeArray();
+
 
 function animate(){
-  context.clearRect(0,0,width,height)
   requestAnimationFrame(animate)
-  atoms.map((atom,i)=>{
-    atoms.map(atm => {
-    atom.draw(context)
-
-      if(atom != atm){
-        let distance;
-        atom.rad.differenceVector(atm.pos,atom.pos);
-        atm.rad.differenceVector(atom.pos,atm.pos);
-        distance = atom.rad.magnitude;
-        if(distance < 2*atom.radius){
-          atom.rad.magnitude = 1;
-          atm.rad.magnitude = 1;
-
-          atom.tan.perpendicular(atom.rad);
-          atm.tan.perpendicular(atm.rad);
-
-          atom.rad.magnitude = atom.rad.dot(atom.vel);
-          atom.tan.magnitude = atom.tan.dot(atom.vel);
-
-          atm.rad.magnitude = atm.rad.dot(atm.vel);
-          atm.tan.magnitude = atm.tan.dot(atm.vel);
-
-          let temp = {};
-          temp.dx =atom.rad.dx;
-          temp.dy = atom.rad.dy;
-
-          atom.rad.dx = atm.rad.dx
-          atom.rad.dy = atm.rad.dy;
-
-          atm.rad.dx = temp.dx;
-          atm.rad.dy = temp.dy;
-
-          atom.vel.sumVector(atom.rad,atom.tan);
-          atm.vel.sumVector(atm.rad,atm.tan);
-
-
-          // atom.rad.draw(context,atom.pos,50,"white")
-          // atom.tan.draw(context,atom.pos,50,"white")
-        }
+  context.clearRect(0,0,width,height);
+  molecules.map((molecule)=>{
+    molecule.update();
+    molecule.draw(context);
+    molecules.map((otherMolecule)=>{
+      if(molecule.index != otherMolecule.index){
+        let distance = new Vector2d(1,1);
+        distance.differenceVector(otherMolecule.pos,molecule.pos);
+        distance.draw(context,molecule.pos,1,"rgba(255,255,255,0.1)")
       }
-    })
-    atom.update();
 
+    });
   })
 }
 
 animate();
 
-
-function createAtoms(){
+function fillMoleculeArray(){
   let array = [];
-  let columnWidth = 100;
+  let numberOfMolecules = 10;
+  let collumnWidth = 100;
   let rowHeight = 100;
+  let moleculesOnARow = 5;
 
-  let numOnARow = Math.floor(width/columnWidth);
-  let numberOfAtoms = numOnARow * Math.floor(height/rowHeight);
-
-  for(let i = 0; i<numberOfAtoms; i++){
-    let x = columnWidth/2 + i%numOnARow * columnWidth;
-    let y = rowHeight/2 + Math.floor(i/numOnARow) * rowHeight;
-    let atom = new dPoint(new Vector2d(x,y),new Vector2d(getRandomInt(-2,2),getRandomInt(-2, 2)),new Vector2d(0,0),20,"white","" + i);
-    atom.rad = new Vector2d(1,1);
-    atom.tan = new Vector2d(1,1);
-    array.push(atom);
+  for(let i=0; i<numberOfMolecules; i++){
+    let x = collumnWidth/2 + i%moleculesOnARow * collumnWidth;
+    let y = rowHeight/2 + Math.floor(i/moleculesOnARow) * rowHeight;
+    let molecule = new dPoint(new Vector2d(x,y),new Vector2d(getRandomNumber(-2,2),getRandomNumber(-2,2)),new Vector2d(0,0),20,"yellow","" + i);
+    molecule.nextPos = new Vector2d(1,1);
+    molecule.index = i;
+    array.push(molecule);
   }
+
   return array;
 }
 
-function getRandomInt(min, max) {
+function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
---
+++ html/a23idealGas/script.js
@@ -0,0 +1,90 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let atoms = createAtoms();
+
+function animate(){
+  context.clearRect(0,0,width,height)
+  requestAnimationFrame(animate)
+  atoms.map((atom,i)=>{
+    atom.draw(context)
+
+    atoms.map(atm => {
+
+      if(atom != atm){
+        let distance;
+        atom.nextPosition.sumVector(atom.pos,atom.vel)
+        atom.rad.differenceVector(atm.pos,atom.nextPosition);
+        atm.rad.differenceVector(atom.nextPosition,atm.pos);
+        distance = atom.rad.magnitude;
+        if(distance < 2*atom.radius){
+          atom.rad.magnitude = 1;
+          atm.rad.magnitude = 1;
+
+          atom.tan.perpendicular(atom.rad);
+          atm.tan.perpendicular(atm.rad);
+
+          atom.rad.magnitude = atom.rad.dot(atom.vel);
+          atom.tan.magnitude = atom.tan.dot(atom.vel);
+
+          atm.rad.magnitude = atm.rad.dot(atm.vel);
+          atm.tan.magnitude = atm.tan.dot(atm.vel);
+
+          let temp = {};
+          temp.dx =atom.rad.dx;
+          temp.dy = atom.rad.dy;
+
+          atom.rad.dx = atm.rad.dx
+          atom.rad.dy = atm.rad.dy;
+
+          atm.rad.dx = temp.dx;
+          atm.rad.dy = temp.dy;
+
+          atom.vel.sumVector(atom.rad,atom.tan);
+          atm.vel.sumVector(atm.rad,atm.tan);
+
+
+          // atom.rad.draw(context,atom.pos,50,"white")
+          // atom.tan.draw(context,atom.pos,50,"white")
+        }
+      }
+    })
+    atom.update();
+
+  })
+}
+
+animate();
+
+
+function createAtoms(){
+  let array = [];
+  let columnWidth = 100;
+  let rowHeight = 100;
+
+  let numOnARow = Math.floor(width/columnWidth);
+  let numberOfAtoms = numOnARow * Math.floor(height/rowHeight);
+
+  for(let i = 0; i<numberOfAtoms; i++){
+    let x = columnWidth/2 + i%numOnARow * columnWidth;
+    let y = rowHeight/2 + Math.floor(i/numOnARow) * rowHeight;
+    let atom = new dPoint(new Vector2d(x,y),new Vector2d(getRandomInt(-2,2),getRandomInt(-2, 2)),new Vector2d(0,0),20,"white","" + i);
+    atom.rad = new Vector2d(1,1);
+    atom.tan = new Vector2d(1,1);
+    atom.nextPosition = new Vector2d(1,1);
+    array.push(atom);
+  }
+  return array;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
