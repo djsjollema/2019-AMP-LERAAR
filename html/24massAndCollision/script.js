d0b83d7f4aec0fcd98d7e13971c4f0b85b3ae4b4
Jelle Sjollema <=>
2020-04-22 12:24:50 +0200

    24 --wrong

--- html/24massAndCollision/script.js
+++ html/24massAndCollision/script.js
@@ -9,8 +9,8 @@ canvas.height = height;
 
 let A,B,difference;
 
-A = new dPoint(new Vector2d(200,200),new Vector2d(5,7),new Vector2d(0,0),50,"yellow","A");
-B = new dPoint(new Vector2d(500,400),new Vector2d(-6,-4),new Vector2d(0,0),250,"blue","B");
+A = new dPoint(new Vector2d(200,200),new Vector2d(1,2),new Vector2d(0,0),50,"yellow","A");
+B = new dPoint(new Vector2d(500,400),new Vector2d(-2,-1),new Vector2d(0,0),250,"blue","B");
 difference = new Vector2d(1,1);
 
 //add mass
@@ -61,7 +61,46 @@ function animate(){
   B.tan.dx = -B.rad.dy
   B.tan.dy = B.rad.dx
 
+  A.tan.magnitude = 1;
+  B.tan.magnitude = 1;
+
   if(difference.magnitude<A.radius + B.radius){
+
+  let Msum = A.mass + B.mass
+  let MAB =A.mass - B.mass
+  let MBA = B.mass - A.mass
+
+  //  P = MAB/Msum * A.rad
+  // Q = 2*B.mass /Msum * B.rad
+  // R = 2*A.mass / Msum * A.rad
+  // S = MBA/Msum * B.rad
+
+  P = new Vector2d(1,1);
+  Q = new Vector2d(1,1);
+  R = new Vector2d(1,1);
+  S = new Vector2d(1,1);
+
+  P.equals(A.rad);
+  Q.equals(B.rad);
+  R.equals(A.rad);
+  S.equals(B.rad);
+
+  P.scalMul(MAB/Msum);
+  Q.scalMul(2*B.mass /Msum);
+  R.scalMul(2*A.mass / Msum);
+  S.scalMul(MBA/Msum);
+
+  // A.rad = P + Q
+  // B.rad = R + S
+
+  A.rad.sumVector(P,Q);
+  B.rad.sumVector(R,S);
+
+  A.vel.sumVector(A.rad,A.tan);
+  B.vel.sumVector(B.rad,B.tan);
+
+
+
     //difference.draw(context,A.pos,1,"red");
   } else {
     //difference.draw(context,A.pos,1,"green");
