9f07662157b2d74f8c7cc4a5e2e7dfb3e57e05aa
Jelle Sjollema <=>
2020-05-25 13:47:19 +0200

    27 --start

---
+++ html/27gravity/script.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+//start here programming
+let earth, moon, distanceEarthMoon,distanceMoonEarth;
+
+earth = new dPoint(new Vector2d(width/2,height/2),new Vector2d(0,1),new Vector2d(0,0),50,"blue","earth");
+moon = new dPoint(new Vector2d(width/3,height/3),new Vector2d(0,0),new Vector2d(0,0),20,"gray","moon");
+distanceEarthMoon = new Vector2d(0,0)
+distanceMoonEarth = new Vector2d(0,0)
+
+function animate(){
+  context.clearRect(0,0,width,height);
+  //begin animation loop
+
+  distanceEarthMoon.differenceVector(moon.pos,earth.pos);
+  distanceMoonEarth.differenceVector(earth.pos,moon.pos);
+
+  distanceEarthMoon.magnitude = 0.5;
+  distanceMoonEarth.magnitude = 0.5;
+
+  earth.acc.equals(distanceEarthMoon);
+  moon.acc.equals(distanceMoonEarth)
+
+
+  earth.update();
+  moon.update();
+  earth.draw(context);
+  moon.draw(context)
+
+  distanceEarthMoon.draw(context,earth.pos,50,"rgba(255,255,255,0.4)")
+  distanceMoonEarth.draw(context,moon.pos,50,"rgba(255,255,0,0.4)")
+}
+
+setInterval(animate,10)
